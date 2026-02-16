f9e757e7eb40c84b55f07d9dc3d6e6599ddda636
mpopova-volt <mpopova@voltactivedata.com>
2026-02-16 16:38:14 -0500

    ENG-28691: adding Client2; updated all tests to use Client2 and kept … (#60)
    * ENG-28691: adding Client2; updated all tests to use Client2 and kept the client1 version in a "client1" sub-package

--- volt-test-container-test/src/test/java/voter/client1/VoltDBClusterSecurityTest.java
+++ volt-test-container-test/src/test/java/voter/client1/VoltDBClusterSecurityTest.java
@@ -5,12 +5,12 @@
  * license that can be found in the LICENSE file or at
  * https://opensource.org/licenses/MIT.
  */
-package voter;
+package voter.client1;
 
 import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.voltdbtest.testcontainer.VoltDBCluster;
+import voter.TestBase;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -21,7 +21,7 @@ import java.nio.file.Path;
 import static org.junit.Assert.*;
 
 public class VoltDBClusterSecurityTest extends TestBase {
-    
+
     private VoltDBCluster cluster;
 
     @After
@@ -30,11 +30,11 @@ public class VoltDBClusterSecurityTest extends TestBase {
             cluster.shutdown();
         }
     }
-    
+
     @Test
     public void testWithDeploymentResource() throws IOException {
         cluster = new VoltDBCluster(validLicensePath, VOLTDB_IMAGE);
-        
+
         // Create a deployment XML file
         Path tempDeployment = Files.createTempFile("deployment", ".xml");
         try (FileWriter writer = new FileWriter(tempDeployment.toFile())) {
@@ -46,7 +46,7 @@ public class VoltDBClusterSecurityTest extends TestBase {
             writer.write("    </paths>\n");
             writer.write("</deployment>");
         }
-        
+
         try {
             VoltDBCluster result = cluster.withDeploymentResourceFromHostPath(tempDeployment.toAbsolutePath().toString());
             assertSame("Should return same cluster instance", cluster, result);
@@ -56,43 +56,43 @@ public class VoltDBClusterSecurityTest extends TestBase {
             Files.delete(tempDeployment);
         }
     }
-    
+
     @Test
     public void testSecureClusterConfiguration() throws IOException {
         cluster = new VoltDBCluster(validLicensePath, "voltdb/voltdb-enterprise:14.1.0");
-        
+
         // Test chaining multiple security configurations
         VoltDBCluster result = cluster
             .withUserNameAndPassword("admin", "admin123")
             .withDeploymentContent("<?xml version=\"1.0\"?><deployment><cluster kfactor=\"0\"/></deployment>")
             .withKsaftey(0);
-            
+
         assertSame("Should return same cluster instance after chaining", cluster, result);
-        
+
         // Start the cluster with security configuration
         cluster.start();
-        
+
         // Verify cluster is running
-        assertNotNull("Client2 should not be null", cluster.getClient2());
+        assertNotNull("Client should not be null", cluster.getClient());
     }
-    
+
     @Test
     public void testMultiNodeSecureCluster() throws IOException {
         // Create a 3-node cluster with security configurations
         cluster = new VoltDBCluster(validLicensePath, "voltdb/voltdb-enterprise:14.1.0", 3, 1);
-        
+
         // Apply security configurations
         cluster
             .withUserNameAndPassword("testuser", "testpass")
             .withDeploymentContent("<?xml version=\"1.0\"?><deployment><cluster kfactor=\"1\"/></deployment>");
-        
+
         // Start the secure cluster
         cluster.start();
-        
+
         // Verify all nodes started
         assertEquals("Should have 3 containers", 3, cluster.getContainerIds().size());
         assertEquals("Should have 3 container names", 3, cluster.getContainerNames().size());
-        
+
         // Verify network is shared
         String networkId = cluster.getNetworkId();
         assertNotNull("Network ID should not be null", networkId);
